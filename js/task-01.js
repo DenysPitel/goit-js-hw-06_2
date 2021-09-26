8f0e0df3f16e16baf185a9fe2e153d3dd02b3bcf
Denys Pitel <nk120788pda@gmail.com>
2021-09-26 23:28:34 +0300

    hw1

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,7 @@
+const categoriesRef = document.querySelectorAll(".item");
+    console.log(`В списке ${categoriesRef.length} категории.`);
 
+categoriesRef.forEach(elem => {
+    console.log(`Категория: ${elem.firstElementChild.textContent}`)
+    console.log(`Количество элементов: ${elem.lastElementChild.children.length}`)
+});
