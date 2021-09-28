60c7146babee8be191a091781eb7e4f7880ad7f9
Denys Pitel <nk120788pda@gmail.com>
2021-09-28 10:08:51 +0300

    hw-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
     'Зелень',
     'Приправы',
 ];
+
+const listEl = document.querySelector('#ingredients');
+
+
+const elements = ingredients.map(ingredient => {
+    const listEl = document.createElement('li');
+    listEl.textContent = ingredient;
+
+    return listEl;
+});
+
+console.log(elements);
+listEl.append(...elements);
